98ad32258ca8df8d473500fc6fe1f612549d2fdb
Paul Fruitful <fruitful2007@outlook.com>
2024-08-25 18:04:24 +0100

    Process Video Endpoint Complete

--- app/api/process-video/route.ts
+++ app/api/process-video/route.ts
@@ -1,25 +1,41 @@
 import { NextResponse } from 'next/server';
 import ffmpeg from 'fluent-ffmpeg';
-import { promises as fs } from 'fs';
+import { unlink } from 'fs';
 import path from 'path';
-const ffmpegPath = 'C:\\ffmpeg\\ffmpeg-2024-08-21-git-9d15fe77e3-essentials_build\\bin\\ffmpeg.exe'; // Update this path to where ffmpeg is installed
+import fs from 'fs';
+import fetch from 'node-fetch';
+import { v4 as uuidv4 } from 'uuid';
+
+// Set the path to the local ffmpeg binary
+const ffmpegPath = 'C:\\ffmpeg\\ffmpeg-2024-08-21-git-9d15fe77e3-essentials_build\\bin\\ffmpeg.exe';
 ffmpeg.setFfmpegPath(ffmpegPath);
+
 export async function POST(request) {
   try {
     const { videoPath } = await request.json();
 
     if (!videoPath) {
-      throw new Error('Video path is not provided');
+      throw new Error('Video URL is not provided');
     }
 
+    const tempFileName = uuidv4() + '.mp4';
+    const tempFilePath = path.join(process.cwd(), 'tmp', tempFileName);
     const outputDirectory = path.join(process.cwd(), 'public', 'audio');
     const outputPath = path.join(outputDirectory, 'output.mp3');
 
-    await fs.mkdir(outputDirectory, { recursive: true });
+    await fs.promises.mkdir(path.dirname(tempFilePath), { recursive: true });
+    await fs.promises.mkdir(outputDirectory, { recursive: true });
+
+    const response = await fetch(videoPath);
+    if (!response.ok) {
+      throw new Error('Failed to download video');
+    }
+    const buffer = await response.buffer();
+    await fs.promises.writeFile(tempFilePath, buffer);
 
     const processVideo = () =>
       new Promise((resolve, reject) => {
-        ffmpeg(videoPath)
+        ffmpeg(tempFilePath)
           .output(outputPath)
           .on('end', () => resolve('Processing complete'))
           .on('error', (err) => {
@@ -30,10 +46,19 @@ export async function POST(request) {
       });
 
     const result = await processVideo();
+
+    unlink(tempFilePath, (err) => {
+        if (err) {
+          console.error('Error deleting temporary file:', err);
+        } else {
+          console.log('Temporary file deleted successfully');
+        }
+      });
+      
     return NextResponse.json({ message: result });
 
   } catch (error) {
-    console.error('Error in video processing endpoint:', error); 
+    console.error('Error in video processing endpoint:', error);
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
 }
